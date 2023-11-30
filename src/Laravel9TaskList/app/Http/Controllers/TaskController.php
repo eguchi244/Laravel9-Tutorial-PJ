135034465683c8fd24ce68cd58699ade19a7fc06
‘eguchi244‘ <mit.liebe0625biz@gmail.com>
2023-11-30 13:35:11 +0900

    Chapter04/ToDoアプリのタスク一覧表示機能を作るを修正

--- src/Laravel9TaskList/app/Http/Controllers/TaskController.php
+++ src/Laravel9TaskList/app/Http/Controllers/TaskController.php
@@ -18,13 +18,13 @@ class TaskController extends Controller
     {
         $folders = Folder::all();
 
-        $current_folder = Folder::find($id);
+        $folder = Folder::find($id);
 
-        $tasks = $current_folder->tasks()->get();
+        $tasks = $folder->tasks()->get();
 
         return view('tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $current_folder->id,
+            'folder_id' => $folder->id,
             'tasks' => $tasks
         ]);
     }
