b5dff1632bde547ffc1c4ea69a72006ebeb036c5
‘eguchi244‘ <mit.liebe0625biz@gmail.com>
2023-12-03 19:20:10 +0900

    Chapter12/ToDoアプリの認証機能を作るPart2を修正

--- src/Laravel9TaskList/database/seeders/TasksTableSeeder.php
+++ src/Laravel9TaskList/database/seeders/TasksTableSeeder.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Seeder;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Folder;
 
 class TasksTableSeeder extends Seeder
 {
@@ -22,6 +24,20 @@ class TasksTableSeeder extends Seeder
             DB::table('tasks')->insert([
                 'folder_id' => 1,
                 'title' => "サンプルタスク {$num}",
+                'status' => $num,
+                'due_date' => Carbon::now()->addDay($num),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+
+        $user = DB::table('users')->where('id', 2)->first();
+        $folder = DB::table('folders')->where('user_id', $user->id)->first();
+
+        foreach (range(1, 3) as $num) {
+            DB::table('tasks')->insert([
+                'folder_id' => $folder->id,
+                'title' => "サンプルタスク {$num}（test2）",
                 'status' => $num,
                 'due_date' => Carbon::now()->addDay($num),
                 'created_at' => Carbon::now(),
